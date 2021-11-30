63f44be4ad8fad5b89a1ffa8d70d3a320d6ae466
vip7009pro <hung1893@gmail.com>
2021-11-30 17:45:42 +0700

    Chỉnh sửa thêm

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -2,10 +2,9 @@ import React from 'react'
 import '../Tabs/Tabs.css'
 import FormDangKyNghi from './FormDangKyNghi/FormDangKyNghi'
 export default function Tabs() {
-    return (
-        <div>
-            <FormDangKyNghi/>
-                                       
-        </div>
+    return (    
+        <div id="dkn">
+            <FormDangKyNghi/> 
+        </div>   
     )
 }
