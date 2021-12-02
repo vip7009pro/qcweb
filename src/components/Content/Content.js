2715a90ea28a1b3a02b486334ebb55fa23d8a3b1
vip7009pro <hung1893@gmail.com>
2021-12-02 13:47:30 +0700

    Them Menu

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import NavMenu from '../NavMenu/NavMenu'
 import Tabs from '../Tabs/Tabs'
 import '../Content/Content.css'
+
 export default function Content() {
     return (
-        <div id="content">
-            {/* <NavMenu/> */}
-            <Tabs/>
+        <div id="content">            
+            <Tabs TabName="1"/>
         </div>
     )
 }
--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -9,19 +9,46 @@ import FormHuongDanSuDung from './FormHuongDanSuDung/FormHuongDanSuDung'
 import FormLichSuDiLam from './FormLichSuDiLam/FormLichSuDiLam'
 import FormLichSuNghi from './FormLichSuNghi/FormLichSuNghi'
 import FormSetTeam from './FormSetTeam/FormSetTeam'
-export default function Tabs() {
-    const [showForm,setShowForm] = useState(1);
+import NavMenu from '../NavMenu/NavMenu'
 
-    return (    
-        <div id="dkn" className="container">            
-            <FormDangKyNghi/> 
-            <FormDangKyTangCa/>
-            <FormLichSuNghi/>
-            <FormLichSuDiLam/>
-            <FormDiemDanhNhom/>
-            <FormDiemDanhTong/>
-            <FormSetTeam/>
-            <FormHuongDanSuDung/>
-        </div>   
+
+function Rd()
+{
+    return (
+        <div>
+            <h1>OK MA</h1>
+        </div>
     )
+}
+export default function Tabs(props) {
+    const [formNo,setFormNo] = useState(props.TabName);
+
+    switch (formNo) {
+        case '1':
+            return (<div id="dkn" className="container">  <Rd/> <NavMenu/> <FormDangKyNghi /> </div>);
+            break;
+        case '2':
+            return (<div id="dkn" className="container">  <NavMenu/> <FormDangKyTangCa /> </div>);
+            break;
+        case '3':
+            return (<div id="dkn" className="container">  <NavMenu/> <FormLichSuNghi /> </div>);
+            break;
+        case '4':
+            return (<div id="dkn" className="container">  <NavMenu/> <FormLichSuDiLam /> </div>);
+            break;
+        case '5':
+            return (<div id="dkn" className="container">  <NavMenu/> <FormDiemDanhNhom /> </div>);
+            break;
+        case '6':
+            return (<div id="dkn" className="container">  <NavMenu/> <FormDiemDanhTong /> </div>);
+            break;
+        case '7':
+            return (<div id="dkn" className="container">  <NavMenu/> <FormSetTeam /> </div>);
+            break;
+        case '8':
+            return (<div id="dkn" className="container">  <NavMenu/> <FormHuongDanSuDung /> </div>);
+            break;
+        default:
+            break;
+    }    
 }
