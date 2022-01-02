1d6ea6386365640bc492bf7301e7ba2f9ff14ba3
vip7009pro <hung1893@gmail.com>
2022-01-02 16:36:18 +0700

    Banner cho QC APP

--- src/components/QC_Component/QC.js
+++ src/components/QC_Component/QC.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import Draggable from 'react-draggable'
+import Banner from '../Banner/Banner'
+import InfoPanel from '../InfoPanel/InfoPanel'
 import PQC from './PQC/PQC'
 export default function QC() {
     return (
-        <div> 
+        <div className='container'> 
+            <Banner/>
+            <InfoPanel/>
             <PQC/>               
         </div>
     )
