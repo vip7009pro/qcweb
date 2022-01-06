27946bfdbf6e6c077872e31b84b35646b7f26b27
vip7009pro <hung1893@gmail.com>
2022-01-06 10:30:51 +0700

    Hàm dùng chung global

---
+++ src/Api/GlobalFunctions.js
@@ -0,0 +1,19 @@
+import moment from "moment";
+import swal from "sweetalert";
+
+export const converttoDateTimefromTime = (ipdate, iptime) => {
+    let output;
+    try
+    {
+        output = ipdate + " " + iptime.substring(0, 2) + ":" + iptime.substring(2, 4);
+        if(moment(output).isValid())
+        return output;
+        swal("Lỗi","Lỗi định dạng ngày tháng, nhập cho đúng định dạng vào","error");
+    }
+    catch(ex)
+    {
+        swal("Lỗi","Lỗi định dạng ngày tháng, nhập cho đúng định dạng vào","error");
+    }
+     
+    
+}
